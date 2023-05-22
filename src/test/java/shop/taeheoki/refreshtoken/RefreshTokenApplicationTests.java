1674d377ad3db037e5807bcd33d573089e6fe69b
taeheoki <dkqjwl2365@naver.com>
2023-05-23 02:16:49 +0900

    🏗config: Refresh Token 준비

---
+++ src/test/java/shop/taeheoki/refreshtoken/RefreshTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.taeheoki.refreshtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
